86ec90e0046ac36089033b7d50b7045f966873e5
0x80090010 <0x90090010@gmail.com>
2025-03-13 01:21:38 +0900

    タイトル画面背景の画像表示

--- Assets/Scenes/TitleSceneManager.cs
+++ Assets/Scenes/TitleSceneManager.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TitleSceneManager : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
-        
+        SetTitleBackground();
     }
 
-    // Update is called once per frame
-    void Update()
+    void SetTitleBackground()
     {
-        
+        //canvas用のゲームオブジェクトを作成
+        GameObject titleBackgroundCanvasObject = new GameObject("TitleBackground");
+        //Canvasコンポーネントを追加
+        Canvas titleBackgroundCanvas = titleBackgroundCanvasObject.AddComponent<Canvas>();
+        //レンダーモードをScreenSpaceCameraに設定
+        titleBackgroundCanvas.renderMode = RenderMode.ScreenSpaceCamera;
+        titleBackgroundCanvasObject.AddComponent<GraphicRaycaster>();
+
+        //image用のゲームオブジェクトを作成
+        GameObject titleBackgroundImageObject = new GameObject("TitleBackgroundImage");
+        //imageコンポーネントを追加
+        Image titleBackgroundImage = titleBackgroundImageObject.AddComponent<Image>();
+        //canvasの子要素に設定
+        titleBackgroundImageObject.transform.SetParent(titleBackgroundCanvas.transform, false);
+        // RectTransformの設定(ウィンドウ全体に表示する)
+        RectTransform rectTransform = titleBackgroundImageObject.GetComponent<RectTransform>();
+        rectTransform.anchorMin = Vector2.zero;
+        rectTransform.anchorMax = Vector2.one;
+        rectTransform.offsetMin = Vector2.zero;
+        rectTransform.offsetMax = Vector2.zero;
+
+        //仮の背景
+        Texture2D titleBackgroundTexture = Resources.Load<Texture2D>("defaultTitleBackground");
+        Sprite sprite = Sprite.Create(titleBackgroundTexture, new Rect(0, 0, titleBackgroundTexture.width, titleBackgroundTexture.height), new Vector2(0.5f, 0.5f));
+        titleBackgroundImage.sprite = sprite;
     }
+
 }
